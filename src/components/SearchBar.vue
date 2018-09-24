46e729cede44596c4eaa92fcd0c9810b815e1ae6
Haris Adilovic <adilovicharis94@gmail.com>
2018-09-25 00:35:04 +0200

    updated files

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input @input="onInput" placeholder="Search for a video . . ." autofocus/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+    onInput(event) {
+      this.$emit('termChange', event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+input {
+  width: 70%;
+  margin-bottom: 2%;
+  border-radius: 5px;
+  height: 40px;
+  font-size: 1.1em;
+}
+div {
+  text-align:  center;
+  margin: 20px;
+}
+</style>
+
